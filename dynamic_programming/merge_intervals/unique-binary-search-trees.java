4d939116e7cf6a32e324933f6f96a71225e05b64
rzsun <zhensn@gmail.com>
2022-03-23 10:29:30 -0400

    Update unique-binary-search-trees.java

--- dynamic_programming/merge_intervals/unique-binary-search-trees.java
+++ dynamic_programming/merge_intervals/unique-binary-search-trees.java
@@ -1,10 +1,13 @@
 class Solution {
     public int numTrees(int n) {
         // for each node, numTrees = numTrees(left) * numTrees(right)
-        int[] dp = new int[n + 1];
-        return numTreesRecursive(dp, n);
+        //int[] dp = new int[n + 1];
+        //return numTreesRecursive(dp, n);
+        
+        return numTrees2(n);
     }
     
+    // top down
     public int numTreesRecursive(int[] dp, int n) {
         if (dp[n] != 0) {
             return dp[n];
@@ -18,5 +21,19 @@ class Solution {
         }
         dp[n] = sum;
         return sum;
+    }
+    
+    // bottom up
+    public int numTrees2(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j > 0; j--) {
+                sum += dp[j - 1] * dp[i - j];
+            }
+            dp[i] = sum;
+        }
+        return dp[n];
     }
 }
