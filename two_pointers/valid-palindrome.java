d64a6577e806efdd576e8334ffe4d93d08d03c06
rzsun <zhensn@gmail.com>
2022-04-04 15:01:44 -0400

    Create valid-palindrome.java

---
+++ two_pointers/valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^A-Za-z0-9]", "");
+        
+        int low = 0;
+        int high = s.length() - 1;
+        
+        while (low <= high) {
+            if (s.charAt(low) != s.charAt(high)) {
+                return false;
+            }
+            low++; high--;
+        }
+        return true;
+    }
+}
