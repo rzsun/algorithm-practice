dbac658bcb9584293c42c42fdece7f8a00f70979
rzsun <zhensn@gmail.com>
2022-04-08 09:17:37 -0400

    Create balanced-binary-tree.java

---
+++ tree/balanced-binary-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean balanced = true;
+    
+    public boolean isBalanced(TreeNode root) {
+        maxDepth(root);
+        return balanced;
+    }
+    
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        balanced = balanced && (Math.abs(left - right) <= 1);
+        return 1 + Math.max(left, right);
+    }
+}
