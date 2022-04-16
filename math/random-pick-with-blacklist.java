ad333c04c085cd8453f610f77478324861b8dd5f
rzsun <zhensn@gmail.com>
2022-04-16 11:34:39 -0400

    Create random-pick-with-blacklist.java

---
+++ math/random-pick-with-blacklist.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    private Map<Integer, Integer> map;
+    private int n;
+    private int offset;
+    
+    /**
+    n = 4
+    [0, 1, 2, 3]
+    [0, 2] 0 -> 3
+    //2 -> 3
+    
+    */
+    public Solution(int n, int[] blacklist) {
+        this.map = new HashMap<>();
+        this.n = n;
+        this.offset = blacklist.length;
+        
+        Set<Integer> vals = new HashSet<>();
+        for (int i : blacklist) {
+            vals.add(i);
+        }
+        
+        int candidate = n;
+        for (int i = 0; i < offset; i++) {
+            if (blacklist[i] < n - offset) {
+                candidate--;
+                while (vals.contains(candidate)) {
+                    candidate--;
+                }
+                map.put(blacklist[i], candidate);
+            }
+        }
+    }
+    
+    public int pick() {
+        int r = new Random().nextInt(n - offset);
+        if (map.containsKey(r)) {
+            return map.get(r);
+        }
+        return r;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(n, blacklist);
+ * int param_1 = obj.pick();
+ */
