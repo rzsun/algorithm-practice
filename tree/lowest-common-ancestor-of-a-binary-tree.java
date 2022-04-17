f6acc709d36ea86ef6142b1b64543c0175ec6dc8
rzsun <zhensn@gmail.com>
2022-04-17 11:35:03 -0400

    Update lowest-common-ancestor-of-a-binary-tree.java

--- tree/lowest-common-ancestor-of-a-binary-tree.java
+++ tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,33 +8,31 @@
  * }
  */
 class Solution {
-    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) {
             return null;
         }
         TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
         if (left != null) {
             return left;
-        } else if (right != null) {
-            return right;
-        } else {
-            if (find(root, p) && find(root, q)) {
-                return root;
-            }
-            return null;
         }
-        
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (right != null) {
+            return right;
+        }
+        if (find(root, p) && find(root, q)) {
+            return root;
+        }
+        return null;
     }
     
-    public boolean find(TreeNode root, TreeNode target) {
-        if (root == null) {
+    public boolean find(TreeNode node, TreeNode target) {
+        if (node == null) {
             return false;
         }
-        if (root == target) {
+        if (node == target) {
             return true;
         }
-        return find(root.left, target) || find(root.right, target);
+        return find(node.left, target) || find(node.right, target);
     }
 }
