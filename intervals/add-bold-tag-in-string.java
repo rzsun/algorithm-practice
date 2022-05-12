6cef3d73a10c91f8c17483ad18e77c3a3bbb5345
rzsun <zhensn@gmail.com>
2022-05-12 11:56:52 -0400

    Update add-bold-tag-in-string.java

--- intervals/add-bold-tag-in-string.java
+++ intervals/add-bold-tag-in-string.java
@@ -1,37 +1,36 @@
 class Solution {
     public String addBoldTag(String s, String[] words) {
-        // start, length
+        
+        // all start and end occurances of words
         List<int[]> intervals = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            for (String word : words) {
-                int start = s.indexOf(word, i);
-                if (start != -1) {
-                    intervals.add(new int[]{start, start+word.length()});
-                }
+        for (String word : words) {
+            int start = s.indexOf(word);
+            while (start != -1) {
+                intervals.add(new int[]{start, start+word.length()});
+                start = s.indexOf(word, start+1);
             }
+        }
+        if (intervals.size() == 0) {
+            return s;
         }
         Collections.sort(intervals, (a,b) -> a[0] - b[0]);
-
+        
         List<int[]> merged = new ArrayList<>();
-        Iterator<int[]> it = intervals.iterator();
-        if (it.hasNext()) {
-            int[] first = it.next();
-            int start = first[0];
-            int end = first[1];
-            while (it.hasNext()) {
-            int[] cur = it.next();
+        int[] first = intervals.get(0);
+        int start = first[0];
+        int end = first[1];
+        for (int i = 1; i < intervals.size(); i++) {
+            int[] cur = intervals.get(i);
             if (cur[0] > end) {
-                    merged.add(new int[]{start, end});
-                    start = cur[0];
-                }
-                end = Math.max(end, cur[1]);
+                merged.add(new int[]{start, end});
+                start = cur[0];
             }
-            merged.add(new int[]{start, end});
+            end = Math.max(end, cur[1]);
         }
-
+        merged.add(new int[]{start, end});
+        
         StringBuilder result = new StringBuilder();
-        int start = 0;
-
+        start = 0;
         for (int[] interval : merged) {
             if (interval[0] - start > 0) {
                 result.append(s.substring(start, interval[0]));
@@ -44,7 +43,6 @@ class Solution {
         if (start < s.length()) {
             result.append(s.substring(start, s.length()));
         }
-
         return result.toString();
     }
 }
