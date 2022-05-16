6aa361e82a0f9d7748ce5ec13b69c59ccac18e38
rzsun <zhensn@gmail.com>
2022-05-16 14:08:56 -0400

    Update longest-increasing-path-in-a-matrix.java

--- dynamic_programming/path/longest-increasing-path-in-a-matrix.java
+++ dynamic_programming/path/longest-increasing-path-in-a-matrix.java
@@ -1,5 +1,6 @@
 class Solution {
     
+    // possible directions
     static final int[][] DIRS = new int[][]{
         {0, -1},
         {0, 1},
@@ -8,31 +9,37 @@ class Solution {
     };
     
     public int longestIncreasingPath(int[][] matrix) {
+        // memoize in cache longest path starting at pos (r,c)
         int[][] cache = new int[matrix.length][matrix[0].length];
         int max = 1;
+        
+        // DFS on every position
         for (int r = 0; r < matrix.length; r++) {
             for (int c = 0; c < matrix[0].length; c++) {
-                max = Math.max(max,
-                               longest(matrix, r, c, cache));
+                max = Math.max(max, dfs(matrix, r, c, cache));
             }
         }
         return max;
     }
     
-    public int longest(int[][] m, 
+    // DFS to find longest increasing path starting at pos (r,c)
+    public int dfs(int[][] m, 
                        int r, 
                        int c,
                        int[][] cache) {
+        // if already cached, return
         if (cache[r][c] != 0) {
             return cache[r][c];
         }
         int max = 0;
+        
+        // DFS in all directions
         for (int i = 0; i < DIRS.length; i++) {
             int r1 = r + DIRS[i][0];
             int c1 = c + DIRS[i][1];
             if (inBounds(m, r1, c1)) {
                 if (m[r1][c1] > m[r][c]) {
-                    max = Math.max(longest(m, r1, c1, cache), max);
+                    max = Math.max(dfs(m, r1, c1, cache), max);
                 }
             }
         }
@@ -41,6 +48,7 @@ class Solution {
         return res;
     }
     
+    // check if position is within grid
     public boolean inBounds(int[][] m, int r, int c) {
         if (r < 0 || r >= m.length) {
             return false;
