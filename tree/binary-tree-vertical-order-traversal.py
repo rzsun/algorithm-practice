8568e7cce53779659944b339ede451069b8f0526
rzsun <zhensn@gmail.com>
2024-04-22 18:06:32 -0400

    Create binary-tree-vertical-order-traversal.py

---
+++ tree/binary-tree-vertical-order-traversal.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import math
+
+
+class Solution(object):
+    def verticalOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root == None:
+            return []
+        map = {}
+        self.traverse(root, map)
+        res = []
+        i = min(map.keys())
+        while i in map.keys():
+            res.append(map[i])
+            i += 1
+        return res
+
+    def traverse(self, root, map):
+        if root == None:
+            return
+        cur = [(root, 0)]
+        next = []
+        while len(cur) > 0:
+            for (node, col) in cur:
+                if col not in map.keys():
+                    map[col] = []
+                map[col].append(node.val)
+                if node.left != None:
+                    next.append((node.left, col-1))
+                if node.right != None:
+                    next.append((node.right, col+1))
+            cur = next
+            next = []
